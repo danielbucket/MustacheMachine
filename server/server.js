c40a8ff7016063732249358ca8e369b218a2bf8f
Daniel Bucket <danieljbucket@gmail.com>
2023-12-26 18:34:19 -0700

    poopinGuy css

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const PORT = 3435
 const ENV = process.env.NODE_ENV || 'dev'
 
 const app = express()
-const config = require(`./webpack.${ENV}.config.js`)
+const config = require(`../webpack.${ENV}.config.js`)
 const compiler = webpack(config)
 
-const appRouter_v1 = require('./server/router')
+const appRouter_v1 = require('./router')
 
 app.use(
 	webpackDevMiddleware(compiler, {
